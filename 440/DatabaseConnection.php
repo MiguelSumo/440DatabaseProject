1996cc8450df12fdb7b1d00d85250d653fb29fb9
Evan Buckingham <112183229+evan-buckingham@users.noreply.github.com>
2025-11-12 08:17:33 -0600

    Update DatabaseConnection.php

--- 440/DatabaseConnection.php
+++ 440/DatabaseConnection.php
@@ -2,7 +2,11 @@
     //Change this to localhost
     define("Conn_Host", "winhost");
     define("Conn_Username", "db_datareader");
+    
+    //Change password to what you have db_datareader as;
+    //I have it set to "password"
     define("Conn_Password", "password");
     define("Conn_Database", "CIS440CarRentalProject");
 
 ?>
+
