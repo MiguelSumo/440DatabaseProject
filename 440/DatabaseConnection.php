c30e08c224569422107d88144297fd79d6d05f24
Evan Buckingham <112183229+evan-buckingham@users.noreply.github.com>
2025-11-12 08:07:51 -0600

    Add rest of PHP files

---
+++ 440/DatabaseConnection.php
@@ -0,0 +1,6 @@
+<?php
+    define("Conn_Host", "winhost");
+    define("Conn_Username", "db_datareader");
+    define("Conn_Password", "password");
+    define("Conn_Database", "CIS440CarRentalProject");
+?>
